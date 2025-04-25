1bfadaff132d9eea6f07cd78ab51a6fe88b975e1
MakenaMwirigi <makenasharon46@gmail.com>
2025-04-25 12:56:57 +0300

    function contd practice

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+// Function declaration
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function expression
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Function that returns a function
+  function wrapAdjective(wrap = "%") {
+    return function(adjective = "a dedicated programmer") {
+        return `You are ${wrap}${adjective}${wrap}!` ;
+    };
+  }
